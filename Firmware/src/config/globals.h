e8befbd98de1e6b06e280f701502c3aed0a50b26
FHeitmann <felixjulianheitmann@gmail.com>
2021-05-22 22:29:07 +0200

    A little more game logic to the GameEngine - Commit 2

--- Firmware/src/config/globals.h
+++ Firmware/src/config/globals.h
@@ -5,14 +5,20 @@
 
 #pragma once
 
-typedef unsigned int Pin;
+#include <Arduino.h>
+
+typedef unsigned int Pin;   // Typedef for a hardware pin
+typedef size_t HorseIdx;       // Typedef for a horse index
 
 namespace Globals {
 
     namespace GameControl {
 
-        constexpr Pin IntStart =            2;          // Interrupt Pin for the start line trigger
-        constexpr Pin IntFinish =           3;          // Interrupt Pin for the finish line trigger
+        constexpr Pin IntStart          = 2;          // Interrupt Pin for the start line trigger
+        constexpr Pin IntFinish         = 3;          // Interrupt Pin for the finish line trigger
+
+        constexpr Pin StartButton       = 4;
+        constexpr Pin ResetButton       = 5;
 
         constexpr unsigned int nHorses =    2;          // The number of horses in the race
         constexpr Pin horseStartTriggers[nHorses] =     // The trigger pins to check whether a horse is at the starting gate 
--- Firmware/src/modules/GameEngine.h
+++ Firmware/src/modules/GameEngine.h
@@ -4,12 +4,48 @@
 // ######################################################
 
 #include <config/globals.h>
+#include <modules/HorseDriver_ULN2003.h>
 
 class GameEngine
 {
 public:
+
+    /**
+     * @brief GameEngine Constructor
+     * 
+     * 
+     */
     GameEngine();
 
+    /**
+     * Resets all horses to their start position
+     */
+    void reset();
+
+    /**
+     * Starts a game. This means calls to loopCall() will cause progress
+     */
+    void start();
+
+    /**
+     * Stops the game. Further calls to loopCall() will not cause progress 
+     */
+    void stop();
+
+    /**
+     * Returns the HorseIdx with the maximum progress
+     * @return HorseIdx index of the currently winning horse
+     */
+    HorseIdx getWinner();
+
+    /**
+     * Function that gets called every main loop iteration
+     * This calls each horses loopCall function
+     */
+    void loopCall();
+
 private:
+
+    HorseDriver_ULN2003 _horses[Globals::GameControl::nHorses];
 
 };
